cdb21642b519d4d69b4a2ba95f9bd58c4670c061
Didac Oliveira <didac@brainupdaters.net>
2017-01-26 12:24:02 +0100

    timezone update

--- shell/rear_debian.sh
+++ shell/rear_debian.sh
@@ -1,5 +1,9 @@
 echo "Hello from $HOSTNAME at FOSDEM 2017"
 
+echo "$(date) - Adjusting Timezone ..."
+ln -sf /usr/share/zoneinfo/Europe/Brussels /etc/localtime
+echo "Europe/Brussels" > /etc/timezone
+
 echo "$(date) - Refreshing repositories ..."
 sed -i "/ftp/s/.us./.be./g" /etc/apt/sources.list
 apt-get update
