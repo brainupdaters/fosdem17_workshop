436a34fcaf9ee988a419497039fdfaf6a5433625
Didac Oliveira <didac@brainupdaters.net>
2017-01-29 15:41:06 +0100

    Update rear_suse.sh

--- shell/rear_suse.sh
+++ shell/rear_suse.sh
@@ -1,6 +1,11 @@
 echo "Hello from $HOSTNAME at FOSDEM 2017"
 
-echo "Accepting EULA ..."
-touch /home/vagrant/.eula-accepted
+#echo "Accepting EULA ..."
+#touch /home/vagrant/.eula-accepted
+
+echo "$(date) - Refreshing repositories ..."
+zypper rr systemsmanagement-chef
+zypper rr systemsmanagement-puppet
+zypper refresh
 
 echo "End of $HOSTNAME customization ..."
