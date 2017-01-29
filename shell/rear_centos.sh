e1f16cb3c2bd7adefadafd441f9f31268df306f4
Didac Oliveira <didac@brainupdaters.net>
2017-01-29 15:41:49 +0100

    Update rear_centos.sh

--- shell/rear_centos.sh
+++ shell/rear_centos.sh
@@ -3,7 +3,7 @@ echo "Hello from $HOSTNAME at FOSDEM 2017"
 echo "$(date) - Adjusting Timezone ..."
 ln -sf /usr/share/zoneinfo/Europe/Brussels /etc/localtime
 
-echo "Restarting network service ..."
+echo "$(date) - Restarting network service ..."
 systemctl restart network
 
 echo "$(date) - Refreshing repositories ..."
