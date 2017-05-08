3465df16588c017b0957aa7c69591280e6fb7709
Didac Oliveira <didac@brainupdaters.net>
2017-05-08 15:22:46 +0200

    Change downloading package to build from master branch

--- shell/drlm_debian.sh
+++ shell/drlm_debian.sh
@@ -13,20 +13,21 @@ apt-get update
 apt-get -y install openssh-client openssl wget gzip tar gawk sed grep coreutils util-linux nfs-kernel-server rpcbind isc-dhcp-server tftpd-hpa syslinux apache2 qemu-utils sqlite3
 apt-get -y install lsb-release
 
-#echo "$(date) - Installing Build deps ..."
-#apt-get -y install git build-essential debhelper
+echo "$(date) - Installing Build deps ..."
+apt-get -y install git build-essential debhelper
 
-#echo "$(date) - Getting DRLM from source ..."
-#git clone https://github.com/brainupdaters/drlm
-#cd drlm
+echo "$(date) - Getting DRLM from source ..."
+git clone https://github.com/brainupdaters/drlm
+cd drlm
+git checkout -b master origin/master 
 
-#echo "$(date) - Building DRLM ..."
-#make deb
-#cd ..
+echo "$(date) - Building DRLM (master branch) ..."
+make deb
+cd ..
 
-echo "$(date) - Installing DRLM ..."
-wget http://www.drlm.org/downloads/drlm_2.1.0_all.deb
-dpkg -i drlm_2.1.0_all.deb
+echo "$(date) - Installing DRLM (master branch) ..."
+#wget http://www.drlm.org/downloads/drlm_2.1.0_all.deb
+dpkg -i drlm*.deb
 
 echo "$(date) - Configuring loop limits ..."
 sed -i "/GRUB_CMDLINE_LINUX=/s/\"\"/\"max_loop=1024\"/g" /etc/default/grub
